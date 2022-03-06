c07caf5ba71036e1b5f79d5519c492d1c8affcb9
Carrington <hartis.c99@gmail.com>
2022-03-06 01:05:47 -0500

    added another api request for uv index

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-var cityContainer = document.getElementById("city-container");
+// variables to store weather data.
+var cityName = document.getElementById("city-name");
+var sky = document.getElementById("sky");
 var currentWeatherData = document.getElementById("current-weather-data");
-var forecast = document.getElementById("forecast-container");
+var currentTemp = document.getElementById("current-temp");
+var currentWind = document.getElementById("current-wind");
+var currentHumidity = document.getElementById("current-humidity");
+var currentUV = document.getElementById("current-uv");
+var FutureForecastData = document.getElementById("forecast-container");
 var searchButton = document.getElementById("search-button");
 var APIKey = "5cc4264c8766b40941f334fc52eb29b6";
-// variable to hold user input.
-var city;
-// variable to store weather data.
-// var requestURL =
-//   "http://api.openweathermap.org/data/2.5/weather?q=" +
-//   city +
-//   "&appid=" +
-//   APIKey;
-var requestURL =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=41.878113&lon=-87.629799&appid=8a4f71946c01452c9735df61812f9851&units=imperial";
 
-function getAPI() {
-  // The API call.
-  fetch(requestURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      // To make in HTML.
-      var cityName = document.createElement("h2");
-      var currentTemp = document.createElement("li");
-      var currentWind = document.createElement("li");
-      var currentHumidity = document.createElement("li");
-      var currentUV = document.createElement("li");
-      // To give HTML data.
-      cityName.textContent = data.timezone;
-      currentTemp.textContent = data.current.temp + "\u00B0F";
-      currentWind.textContent = data.current.wind_speed + " MPH";
-      currentHumidity.textContent = data.current.humidity + " %";
-      currentUV.textContent = data.current.uvi;
-      // To display HTML onto page.
-      cityContainer.append(cityName);
-      currentWeatherData.append(currentTemp);
-      currentWeatherData.append(currentWind);
-      currentWeatherData.append(currentHumidity);
-      currentWeatherData.append(currentUV);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-}
-searchButton.addEventListener("click", getAPI);
+searchButton.addEventListener("click", function () {
+  var citySearch = document.getElementById("search-input");
+  var requestURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    citySearch.value +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+
+  function getAPI() {
+    // The API call.
+    fetch(requestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        // To give HTML data.
+        cityName.innerHTML = data.name;
+        sky.innerHTML = data.weather[0].description;
+        currentTemp.innerHTML = "Temperature: " + data.main.temp + "\u00B0F";
+        currentWind.innerHTML = "Wind Speed: " + data.wind.speed + " MPH";
+        currentHumidity.innerHTML = "Humidity: " + data.main.humidity + " %";
+        // Another API for UV index.
+        console.log(data.coord.lat);
+        var requestUV =
+          "http://api.openweathermap.org/geo/1.0/direct?q=" +
+          data.coord.lat +
+          data.coord.lon +
+          "&appid=" +
+          APIKey;
+
+        function getUV() {
+          fetch(requestUV)
+            .then(function (response) {
+              console.log(response);
+              console.log(response.json());
+              return response.json();
+            })
+            .then(function (uvData) {
+              console.log(uvData);
+              currentUV.innerHTML = "UV Index: " + uvData.uvi;
+            });
+        }
+        getUV();
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+  getAPI();
+});
