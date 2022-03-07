6a3029bbc3f327346c52440e14f92e23b10c97c8
Carrington <hartis.c99@gmail.com>
2022-03-06 19:55:00 -0500

    changed API to get future weather data

--- script.js
+++ script.js
@@ -6,44 +6,40 @@ var currentTemp = document.getElementById("current-temp");
 var currentWind = document.getElementById("current-wind");
 var currentHumidity = document.getElementById("current-humidity");
 var currentUV = document.getElementById("current-uv");
-var FutureForecastData = document.getElementById("forecast-container");
+var futureForecastData = document.getElementById("forecast-container");
 var searchButton = document.getElementById("search-button");
 var APIKey = "5cc4264c8766b40941f334fc52eb29b6";
 
 searchButton.addEventListener("click", function () {
   var citySearch = document.getElementById("search-input");
   var requestURL =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
     citySearch.value +
     "&appid=" +
-    APIKey +
-    "&units=imperial";
+    APIKey;
 
-  function getAPI() {
-    // The API call.
+  function getWeatherData() {
+    // The geolocation API call that takes the city input as a parameter.
     fetch(requestURL)
       .then(function (response) {
         return response.json();
       })
-      .then(function (data) {
-        console.log(data);
-        // To give HTML data.
-        cityName.innerHTML = data.name;
-        sky.innerHTML = data.weather[0].description;
-        currentTemp.innerHTML = "Temperature : " + data.main.temp + "\u00B0F";
-        currentWind.innerHTML = "Wind Speed : " + data.wind.speed + " MPH";
-        currentHumidity.innerHTML = "Humidity : " + data.main.humidity + " %";
-        function getUV() {
-          console.log(data);
-          var requestUV =
+      .then(function (coordinateData) {
+        // Displays the City's name in HTML.
+        cityName.innerHTML = coordinateData[0].name;
+
+        function forecastWeather() {
+          // Convert the city name parameter into latitude and longitude for Onecall API.
+          var forecastWeather =
             "http://api.openweathermap.org/data/2.5/onecall?lat=" +
-            data.coord.lat +
+            coordinateData[0].lat +
             "&lon=" +
-            data.coord.lon +
+            coordinateData[0].lon +
             "&appid=" +
-            APIKey;
-          console.log(requestUV);
-          fetch(requestUV)
+            APIKey +
+            "&units=imperial";
+          console.log(forecastWeather);
+          fetch(forecastWeather)
             .then(function (response) {
               if (response.ok) {
                 console.log(response);
@@ -52,16 +48,25 @@ searchButton.addEventListener("click", function () {
                 return Promise.reject(response);
               }
             })
+            // Displays data for today's weather onto HTML.
             .then(function (data) {
               console.log(data);
+              sky.innerHTML = data.current.weather[0].description;
+              currentTemp.innerHTML =
+                "Temperature : " + data.current.temp + "\u00B0F";
+              currentWind.innerHTML =
+                "Wind Speed : " + data.current.wind_speed + " MPH";
+              currentHumidity.innerHTML =
+                "Humidity : " + data.current.humidity + " %";
               currentUV.innerHTML = "UV Index: " + data.current.uvi;
+              // Displays weather for the next 5 days.
             });
         }
-        getUV();
+        forecastWeather();
       })
       .catch(function (err) {
         console.log(err);
       });
   }
-  getAPI();
+  getWeatherData();
 });
