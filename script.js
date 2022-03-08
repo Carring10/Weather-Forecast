04344e0c1b256b768de75f3c73d5744debdd1897
Carrington <hartis.c99@gmail.com>
2022-03-07 19:33:01 -0500

    weather for 5 day forecast and img icons for weather conditions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// variables to store weather data.
+// variables to store today's weather data.
 var cityName = document.getElementById("city-name");
 var sky = document.getElementById("sky");
 var currentWeatherData = document.getElementById("current-weather-data");
@@ -6,10 +6,11 @@ var currentTemp = document.getElementById("current-temp");
 var currentWind = document.getElementById("current-wind");
 var currentHumidity = document.getElementById("current-humidity");
 var currentUV = document.getElementById("current-uv");
-var futureForecastData = document.getElementById("forecast-container");
+// var dayContainer = document.getElementById("day-container");
 var searchButton = document.getElementById("search-button");
 var APIKey = "5cc4264c8766b40941f334fc52eb29b6";
 
+var newSearch = 0;
 searchButton.addEventListener("click", function () {
   var citySearch = document.getElementById("search-input");
   var requestURL =
@@ -51,6 +52,11 @@ searchButton.addEventListener("click", function () {
             // Displays data for today's weather onto HTML.
             .then(function (data) {
               console.log(data);
+              var iconCode = data.current.weather[0].icon;
+              var iconURL =
+                "http://openweathermap.org/img/w/" + iconCode + ".png";
+              document.getElementById("weather-icon").src = iconURL;
+
               sky.innerHTML = data.current.weather[0].description;
               currentTemp.innerHTML =
                 "Temperature : " + data.current.temp + "\u00B0F";
@@ -59,7 +65,51 @@ searchButton.addEventListener("click", function () {
               currentHumidity.innerHTML =
                 "Humidity : " + data.current.humidity + " %";
               currentUV.innerHTML = "UV Index: " + data.current.uvi;
+
+              // Set the container that holds the 5 day forecast to an empty string so each iteration of the for loop does not create endless elements.
+              var forecastContainer =
+                document.getElementById("forecast-container");
+              forecastContainer.innerHTML = "";
+
               // Displays weather for the next 5 days.
+              for (let i = 1; i < 6; i++) {
+                // Make HTML for future forecasts.
+                var card = document.createElement("div");
+                var cardDate = document.createElement("h3");
+                var cardTemp = document.createElement("p");
+                var cardWind = document.createElement("p");
+                var cardHumidity = document.createElement("p");
+                // Convert icon code to an image.
+                var cardImg = document.createElement("img");
+                var iconCode = data.daily[i].weather[0].icon;
+                var iconURL =
+                  "http://openweathermap.org/img/w/" + iconCode + ".png";
+                // Convert unix time to date.
+                var date = new Date(data.daily[i].dt * 1000);
+                var formattedDate =
+                  date.getMonth() +
+                  1 +
+                  "/" +
+                  date.getDate() +
+                  "/" +
+                  date.getFullYear();
+                // Give HTML data.
+                cardImg.src = iconURL;
+                cardDate.textContent = formattedDate;
+                cardTemp.textContent =
+                  "Temperature : " + data.daily[i].temp.day + "\u00B0F";
+                cardHumidity.textContent =
+                  "Humidity : " + data.daily[i].humidity;
+                cardWind.textContent =
+                  "Wind : " + data.daily[i].wind_speed + " MPH";
+                // Display onto page.
+                forecastContainer.appendChild(card);
+                card.append(cardImg);
+                card.append(cardDate);
+                card.append(cardTemp);
+                card.append(cardHumidity);
+                card.append(cardWind);
+              }
             });
         }
         forecastWeather();
